819cdc9e4d31419a17582037a67fa3fafd839754
Mustafa Lanewala <125785421+mustafalanewala@users.noreply.github.com>
2024-10-19 21:51:02 +0530

    moved Firebase config to .env for security and environment management

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,14 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIi8pLEBoNJNzHTAlyHkgbaZRfkbyTtgE",
-  authDomain: "mtask-2024.firebaseapp.com",
-  databaseURL: "https://mtask-2024-default-rtdb.firebaseio.com",
-  projectId: "mtask-2024",
-  storageBucket: "mtask-2024.appspot.com",
-  messagingSenderId: "753647947337",
-  appId: "1:753647947337:web:faf2b237c63e9ce37b7998",
-  measurementId: "G-8DPV537TLD"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
